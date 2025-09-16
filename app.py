64359a022b19be1255bf3ec0dc24f3654ae55386
irinkurian <irinkurian2002@gmail.com>
2025-09-16 08:44:31 +0530

    Fix port for Hugging Face Spaces

--- app.py
+++ app.py
@@ -103,5 +103,6 @@ iface = gr.Interface(
 )
 
 if __name__ == "__main__":
-    iface.launch(server_name="0.0.0.0", server_port=8080, share=True)
+    iface.launch(server_name="0.0.0.0", server_port=7860)
+
 
